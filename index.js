26f24bebb91f1801b3da482d92a646157a8b579c
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-16 20:22:03 +0100

    appointment option get api and post booking api

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.removeListener.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Server is Running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const timeSlotConnection = client
+      .db("doctors-portal")
+      .collection("timeSlots");
+
+    const bookingCollection = client
+      .db("doctors-portal")
+      .collection("bookings");
+
+    app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      console.log(date);
+      const query = {};
+      const options = await timeSlotConnection.find(query).toArray();
+      res.send(options);
+    });
+
+    /***
+     * bookings API naming convention
+     * app.get('/bookings)
+     * app.get('/bookings/:id)
+     * app.post('/bookings)
+     * app.patch('/bookings/:id)
+     ***/
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.log());
+
+app.listen(port, () => console.log(`Dpctprs Portal running on ${port}`));
