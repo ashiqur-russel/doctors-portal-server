b1b6cccd73bffa04f0648b9e2eedafd6f9822e8d
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-17 02:48:16 +0100

    save registered user ond atabase

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     const bookingCollection = client
       .db("doctors-portal")
       .collection("bookings");
+    const usersCollection = client.db("doctors-portal").collection("users");
 
     app.get("/appointmentOptions", async (req, res) => {
       const date = req.query.date;
@@ -87,6 +88,15 @@ async function run() {
         return res.send({ acknowledged: false, message });
       }
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    /* Users API */
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
   } finally {
