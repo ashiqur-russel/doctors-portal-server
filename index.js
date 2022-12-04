450c3820417c0aef4cd3a8209add6c3b597d6080
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-04 02:59:21 +0100

    jwt token issuw

--- index.js
+++ index.js
@@ -187,30 +187,16 @@ async function run() {
     });
 
     //add doctor
-    app.post("/doctors", verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
-      const query = { email: decodedEmail };
-      const user = await usersCollection.findOne(query);
-      if (user?.role !== "admin") {
-        return res.status(403).send({ message: "forbidden access" });
-      }
+    app.post("/doctors", async (req, res) => {
       const data = req.body;
       console.log(data);
       const result = await doctorsCollection.insertOne(data);
       res.send(result);
     });
     //get doctor
-    app.get("/doctors", verifyJWT, async (req, res) => {
-      let query = {};
-      const decodedEmail = req.decoded.email;
-      query = { email: decodedEmail };
-      const user = await usersCollection.findOne(query);
-      if (user?.role !== "admin") {
-        return res.status(403).send({ message: "forbidden access" });
-      }
-      const doctorsQuery = {};
-
-      const result = await doctorsCollection.find(doctorsQuery).toArray();
+    app.get("/doctors", async (req, res) => {
+      const query = {};
+      const result = await doctorsCollection.find(query).toArray();
       res.send(result);
     });
   } finally {
